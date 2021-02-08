4d489b197c0d76866fe4274f81cabf9add924447
Joel Lobo <joellobo@gmail.com>
2021-02-08 20:57:12 -0300

    Added letters

---
+++ app1/new-site/js/main.js
@@ -0,0 +1,9 @@
+function sortear(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
