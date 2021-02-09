e90e0eb689f060de790d7143a49c57d31364acb4
Joel Lobo <joellobo@gmail.com>
2021-02-09 12:16:53 -0300

    Added app2

---
+++ app2/src/app.js
@@ -0,0 +1,19 @@
+const app = () => {
+    form()
+}
+
+const form = () => {
+    const app = document.querySelector('.app')
+    const butao = document.createElement('button')
+    app.appendChild(butao)
+}
+
+const layout = () => {
+    const container = document.createElement('container')
+
+
+    app.appendChild(butao)
+}
+
+app()
+
